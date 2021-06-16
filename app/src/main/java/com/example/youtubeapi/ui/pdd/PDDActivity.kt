c1d4ac06875b74aa040c8581d30278d41efa84e0
Altynbek1987 <hyundai8788@gmail.com>
2021-06-16 09:33:09 +0600

    Отключил скрол вю пейжера. По нажатию только перелистывается

--- app/src/main/java/com/example/youtubeapi/ui/pdd/PDDActivity.kt
+++ app/src/main/java/com/example/youtubeapi/ui/pdd/PDDActivity.kt
@@ -1,11 +1,14 @@
 package com.example.youtubeapi.ui.pdd
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View.OnTouchListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.example.youtubeapi.R
 import com.example.youtubeapi.ui.pdd.pager.PagerAdapter
+
 
 class PDDActivity : AppCompatActivity() {
     private val ID_HOME = 1
@@ -13,19 +16,17 @@ class PDDActivity : AppCompatActivity() {
     private val ID_ACCOUNT = 3
     private val ID_MONEY = 4
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pddactivity)
 
         val viewPager = findViewById<ViewPager>(R.id.viewPager)
         viewPager.adapter = PagerAdapter(supportFragmentManager)
-
+        viewPager.setOnTouchListener(OnTouchListener { v, event -> true })
 
         val bottomNavigation =
             findViewById<MeowBottomNavigation>(R.id.bottomNavigation)
-        // Надо соединить bottomNavigation и viewPager
-
-
 
         bottomNavigation.add(MeowBottomNavigation.Model(ID_HOME, R.drawable.ic_car))
         bottomNavigation.add(MeowBottomNavigation.Model(ID_MESSAGE, R.drawable.ic_message))
@@ -39,41 +40,14 @@ class PDDActivity : AppCompatActivity() {
         bottomNavigation.setOnShowListener { item ->
             val name: String
             when (item.id) {
-                ID_HOME -> {name = viewPager.setCurrentItem(0, false).toString()
-                }
+                ID_HOME -> {name = viewPager.setCurrentItem(0, false).toString()}
                 ID_MESSAGE -> {name = viewPager.setCurrentItem(1, false).toString()}
-                ID_MONEY -> {
-                    name = viewPager.setCurrentItem(2, false).toString()
-                }
-                ID_ACCOUNT -> { name = viewPager.setCurrentItem(3, false).toString()
-
-                }
+                ID_MONEY -> { name = viewPager.setCurrentItem(2, false).toString()}
+                ID_ACCOUNT -> { name = viewPager.setCurrentItem(3, false).toString()}
                 else -> name = ""
             }
         }
-
-        // bottomNavigation.setCount(ID_NOTIFICATION,"4");
+         bottomNavigation.setCount(ID_MONEY,"1");
         bottomNavigation.show(ID_HOME, true)
-
-
-        viewPager!!.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                //alerta("menu",position.toString())
-            }
-
-            override fun onPageSelected(position: Int) {
-               // bottomNavigationView!!.getMenu().getItem(position).setChecked(true)
-                findViewById<MeowBottomNavigation>(R.id.bottomNavigation)
-
-
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-
-            }
-        })
-
-
     }
 }
--- app/src/main/java/com/example/youtubeapi/ui/pdd/pager/PagerAdapter.kt
+++ app/src/main/java/com/example/youtubeapi/ui/pdd/pager/PagerAdapter.kt
@@ -3,26 +3,27 @@ package com.example.youtubeapi.ui.pdd.pager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation
-import com.example.youtubeapi.R
 import com.example.youtubeapi.ui.pdd.fragments.fines.FinesFragment
 import com.example.youtubeapi.ui.pdd.fragments.pdd.PddFragment
 import com.example.youtubeapi.ui.pdd.fragments.signs.SignsFragment
 import com.example.youtubeapi.ui.pdd.fragments.tests.TestsFragment
+import java.util.*
 
 class PagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
     override fun getCount(): Int {
         return 4
     }
 
 
     override fun getItem(position: Int): Fragment {
+
        when(position){
            0 -> {
-               return FinesFragment()
+               return PddFragment()
            }
            1 -> {
-               return PddFragment()
+               return FinesFragment()
            }
            2 -> {
                return SignsFragment()
@@ -31,29 +32,8 @@ class PagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
                return TestsFragment()
            }
            else -> {
-               return FinesFragment()
+               return PddFragment()
            }
        }
     }
-
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        when(position) {
-            0 -> {
-                return "Tab 1"
-            }
-            1 -> {
-                return "Tab 2"
-            }
-            2 -> {
-                return "Tab 3"
-            }
-            4 -> {
-                return "Tab 4"
-            }
-        }
-        return super.getPageTitle(position)
-    }
-
-
 }
